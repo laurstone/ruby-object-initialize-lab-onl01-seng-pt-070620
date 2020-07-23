24ee8451021f47ab1e47f38cf549ace96d23dee2
Lauren Stone <lstone321@gmail.com>
2020-07-23 01:35:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog 
-  
-  def initialize(breed)
-    @breed = breed 
-  end 
-  
-  def breed=(breed)
-    @breed = breed 
-  end 
-  
-  def breed 
-    @breed 
-  end 
-end 
+ 
+ def initialize(name, breed) 
+   name = @name
+   breed = @breed
+    if breed = nil 
+      breed = "Mutt"
+    end
+ end
+ 
